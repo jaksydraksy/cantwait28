672dc2d784118873c1c7ef6aa130ce2fcdeea37a
Majam <maja@gmail.com>
2023-01-23 12:59:07 +0100

    Show Snackbar on adding error

--- lib/features/add/cubit/add_cubit.dart
+++ lib/features/add/cubit/add_cubit.dart
@@ -8,8 +8,12 @@ part 'add_state.dart';
 class AddCubit extends Cubit<AddState> {
   AddCubit() : super(const AddState());
 
-  Future<void> add(String title, String imageURL, DateTime releaseDate,) async {
-    try {
+  Future<void> add(
+    String title,
+    String imageURL,
+    DateTime releaseDate,
+  ) async {
+    try {      
       await FirebaseFirestore.instance.collection('items').add(
         {
           'title': title,
@@ -17,6 +21,7 @@ class AddCubit extends Cubit<AddState> {
           'release_date': releaseDate,
         },
       );
+      
       emit(const AddState(saved: true));
     } catch (error) {
       emit(AddState(errorMessage: error.toString()));
--- lib/features/add/page/add_page.dart
+++ lib/features/add/page/add_page.dart
@@ -21,7 +21,19 @@ class _AddPageState extends State<AddPage> {
     return BlocProvider(
       create: (context) => AddCubit(),
       child: BlocListener<AddCubit, AddState>(
-        listener: (context, state) {},
+        listener: (context, state) {
+          if (state.saved) {
+            Navigator.of(context).pop();
+          }
+          if (state.errorMessage.isNotEmpty) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(state.errorMessage),
+                backgroundColor: Colors.red,
+              ),
+            );
+          }
+        },
         child: BlocBuilder<AddCubit, AddState>(
           builder: (context, state) {
             return Scaffold(
